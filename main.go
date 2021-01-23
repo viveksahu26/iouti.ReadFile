20223e703de192f49c54dc014cb556612c4797f2
vivek kumar sahu <vivekkumarsahu650@gmail.com>
2021-01-23 04:13:14 +0000

    Finally changes are made,,

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
+	//"log"
 )
 
+func check(e error) {
+    if e != nil {
+        panic(e)
+    } 
+}
 func main() {
+  //WriteFile writes data to a file named by filename. If the file does not exist,
+  //WriteFile creates it with permissions
+  data:=[]byte("All the data I wish to write to a file name data.txt")
+
+  err:= ioutil.WriteFile("data.txt", data, 0777) 
+  check(err)
+  fmt.Println("Successfully written data into data.txt\n")
+  
   //ReadFile bring file into memory and read whole
   //file at once and return in form of byte to buf.
-	content, err := ioutil.ReadFile("file.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	content, err := ioutil.ReadFile("data.txt")
+	check(err)
   // Converting bytes of data into strings.
-	fmt.Println((string(content)))
+	fmt.Println("Now Reading file Data.txt :-\n"+(string(content)))
 
 }
