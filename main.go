dbb8fdbfa2905281e10755a19c1ae32e1fa6c217
vivek kumar sahu <vivekkumarsahu650@gmail.com>
2021-01-23 03:56:47 +0000

    made changes

--- main.go
+++ main.go
@@ -7,11 +7,13 @@ import (
 )
 
 func main() {
+  //ReadFile bring file into memory and read whole
+  //file at once and return in form of byte to buf.
 	content, err := ioutil.ReadFile("file.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-
+  // Converting bytes of data into strings.
 	fmt.Println((string(content)))
 
 }
